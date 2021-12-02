4eac64a244641b3f0b926d575aa78e319377e24d
Rebecca Nelson <dekarrin.irl@gmail.com>
2021-12-02 16:02:59 -0600

    report errors correctly

--- cf.py
+++ cf.py
@@ -6,7 +6,7 @@ def run_from_cli():
     parser = argparse.ArgumentParser(description="Create vast new worlds by idling")
     parser.add_argument('-s', '--state', default='cre8.p', help="Give location of state file")
     
-    subparsers = parser.add_subparsers(required=True)
+    subparsers = parser.add_subparsers(required=True, dest="command")
     status_parser = subparsers.add_parser('status', help="Show current status of the game")
     status_parser.set_defaults(func=exec_status)
     
