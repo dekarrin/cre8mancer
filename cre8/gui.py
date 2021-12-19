03333da981ea6e5a2b7b853f062dfc288cf09179
Rebecca Nelson <dekarrin.irl@gmail.com>
2021-12-19 11:27:59 -0600

    probably broken but refactored gui to oop

--- cre8/gui.py
+++ cre8/gui.py
@@ -5,100 +5,48 @@ from tkinter import ttk
 
 from typing import Tuple
 
-ActionOptionsList = list()
-ActionOptionsList += ['-- Select Activity --', '', '-- Jobs --']
-ActionOptionsList += [j.name for j in Jobs] + ['-- Outlets --'] + [o.name for o in Outlets]
 
-
-# TODO: this entire module would be much better as a class
-
-def make_act_options_menu(master) -> tk.StringVar:
-    global ActionOptionsList
-        
-    variable = tk.StringVar()
-    variable.set(ActionOptionsList[0])
-    
-    opts_menu = tk.OptionMenu(master, variable, *ActionOptionsList)
-    opts_menu.pack(side=tk.LEFT)
-    opts_menu.config(width=20)
-    return variable
-
-def read_act_options(variable: tk.StringVar) -> Tuple[str, int]:
-    """
-    Read action options that supplies answer to variable and return
-    type and index
-    """
-    val = variable.get()
-    
-    idx = -1
-    for j in Jobs:
-        idx += 1
-        if j.name.lower() == val.lower():
-            return 'job', idx
-    idx = -1
-    for o in Outlets:
-        idx += 1
-        if o.name.lower() == val.lower():
-            return 'outlet', idx
-    
-    return None, 0
-    
-
-def build_click_component(g: Engine, output_func, status_func, master):
-    global mode, modeBtnVar
-    frm_component = tk.Frame(master=master)
-    frm_component.pack(side=tk.TOP, padx=2, pady=2)
-    
-    click_opt_var = make_act_options_menu(frm_component)
-    def do_click():
-        target_type, target_idx = read_act_options(click_opt_var)
-        if target_type is None:
-            output_func("Select a valid option first")
-            return
-        
-        try:
-            msg = g.click(target_type, target_idx)
-        except RulesViolationError as ex:
-            output_func(str(ex))
-            return
-        
-        mode = "status"
-        modeBtnVar.set("Store")
-        output_func(msg)
-        g.update()
-        status_func(g.status())
-
-    entry_click_lbl = tk.Button(frm_component, text="Click!", command=do_click)
-    entry_click_lbl.pack(side=tk.LEFT)
-    
+class ActivitiesOptionsMenu(tk.OptionMenu):
+	def __init__(self, master):
+		self._options_list = list()
+		self._init_options()
+		
+		self._var = tk.StringVar()
+		self._var.set(self._options_list[0])
+		
+		super().__init__(master, self._var, self._options_list)
+		self.config(width=20)
+		
+	def value_as_target(self) -> Tuple[str, int]:
+		"""
+		Read action options that supplies answer to variable and return
+		type and index
+		"""
+		val = self._var.get()
+		
+		idx = -1
+		for j in Jobs:
+			idx += 1
+			if j.name.lower() == val.lower():
+				return 'job', idx
+		idx = -1
+		for o in Outlets:
+			idx += 1
+			if o.name.lower() == val.lower():
+				return 'outlet', idx
+		
+		return None, 0		
+		
+	def _init_options(self):
+		self._options_list = list()
+		self._options_list.append('-- Select Activity --')
+		self._options_list.append('')
+		self._options_list.append('-- Jobs --')
+		self._options_list += [j.name for j in Jobs]
+		self._options_list.append('-- Outlets --')
+		self._options_list += [o.name for o in Outlets]
 
     
-def build_buy_component(g: Engine, output_func, status_func, master):
-    global mode, modeBtnVar
-    frm_component = tk.Frame(master=master)
-    frm_component.pack(side=tk.TOP, padx=2, pady=2)
-    
-    opt_var = make_act_options_menu(frm_component)
-    def do_buy():
-        target_type, target_idx = read_act_options(opt_var)
-        if target_type is None:
-            output_func("Select a valid option first")
-            return
-        
-        try:
-            msg = g.buy('instance', target_type, target_idx)
-        except RulesViolationError as ex:
-            output_func(str(ex))
-            return
-        
-        mode = "status"
-        modeBtnVar.set("Store")
-        output_func(msg)
-        g.update()
-        status_func(g.status())
-
-    entry_click_lbl = tk.Button(frm_component, text="Buy", command=do_buy)
-    entry_click_lbl.pack(side=tk.LEFT)
     
 
     
@@ -114,81 +62,128 @@ def set_mode(new_mode: str):
         modeBtnVar.set("Store")
         if not updating:
             updating = True
-            
-
-def build_mode_component(g: Engine, output_func, status_func, master):
-    global mode, modeBtnVar
     
-    def swap_mode():
-        global mode, modeBtnVar
-        
-        if mode == 'status':
-            mode = "store"
-            modeBtnVar.set("Status")
-        elif mode == 'store':
-            mode = "status"
-            modeBtnVar.set("Store")
+class Gui:
+	def __init__(self, g: Engine, output_lines: int = 7):
+		self.g = g
+		self.root = tk.Tk()
+		self.root.title("Cr8or Forge v0.0a")
+		
+		self.mode_button_var = tk.StringVar()
+		self.mode_button_var.set("Store")
+		self.mode = 'status'
+		
+		# setup root window config
+		self.root.rowconfigure(0, minsize=300, weight=1)
+		self.root.columnconfigure(0, minsize=400, weight=1)
+		self.root.rowconfigure(1, minsize=100, weight=0)
+		
+		# setup main content frame and store it for later outputting
+		frm_main = tk.Frame(master=root, relief=tk.SUNKEN, borderwidth=3)
+		frm_main.grid(row=0, column=0, sticky="nsew")
+		self.main_content = tk.Text(master=frm_main)
+		mc_scrollbar = ttk.Scrollbar(master=frm_main, command=self.main_content.yview)
+		mc_scrollbar.pack(side=tk.RIGHT, fill="y")
+		self.main_content['yscrollcommand'] = mc_scrollbar.set
+		self.main_content.pack(side=tk.RIGHT, fill="x")
+		
+		# setup entry frame
+		frm_entry = tk.Frame(master=root)
+		frm_entry.grid(row=0, column=1, sticky="nsew")
+		self._build_click_component(frm_entry)
+		self._build_buy_component(frm_entry)
+		mode_btn = tk.Button(master, textvariable=modeBtnVar, command=self.swap_mode)
+		mode_btn.pack(side=tk.TOP)
+		
+		# setup up output frame and store it for later outputing
+		frm_output = tk.Frame(master=root)
+		frm_output.grid(row=1, column=0, columnspan=2, sticky="nsew")
+		self.output = tk.Text(master=frm_output, height=output_lines, width=103)
+		self.output.config(state=tk.DISABLED)
+		self.output.pack()
+
+		# do a single update to get window size then set it as the minimum
+		# so user cant resize smaller than the elements
+		root.update()
+		root.minsize(root.winfo_width(), root.winfo_height())
+		
+	def run(self):
+		self.root.mainloop()
+    
+    def swap_mode(self):
+        if self.mode == 'status':
+            self.mode = "store"
+            self.mode_button_var.set("Status")
+        elif self.mode == 'store':
+            self.mode = "status"
+            self.mode_button_var.set("Store")
         else:
             raise ValueError("Should never happen")
-    
-    btn = tk.Button(master, textvariable=modeBtnVar, command=swap_mode)
-    btn.pack(side=tk.TOP)
-    
-    
+		
+	def write_output(self, text: str):
+        self.output.config(state=tk.NORMAL)
+        self.output.delete("0.0", tk.END)
+        self.output.insert("0.0", text)
+        self.output.config(state=tk.DISABLED)
+		
+	def write_main_content(self, text: str):
+        self.main_content.config(state=tk.NORMAL)
+        self.main_content.delete("0.0", tk.END)
+        self.main_content.insert("0.0", text)
+        self.main_content.config(state=tk.DISABLED)
+		
+	def _build_click_component(self, master):
+		frm_component = tk.Frame(master=master)
+		frm_component.pack(side=tk.TOP, padx=2, pady=2)
+		
+		opts_menu = ActivitiesOptionsMenu(frm_component)
+		def do_click():
+			target_type, target_idx = opts_menu.value_as_target()
+			if target_type is None:
+				self.write_output("Select a valid option first")
+				return
+			
+			try:
+				msg = self.g.click(target_type, target_idx)
+			except RulesViolationError as ex:
+				self.write_outpet(str(ex))
+				return
+			
+			self.write_output(msg)
+			self.g.update()
+			self.write_main_content(self.g.status())
 
+		entry_click_lbl = tk.Button(frm_component, text="Click!", command=do_click)
+		entry_click_lbl.pack(side=tk.LEFT)
+
+	def _build_buy_component(self, master):
+		frm_component = tk.Frame(master=master)
+		frm_component.pack(side=tk.TOP, padx=2, pady=2)
+		
+		opts_menu = ActivitiesOptionsMenu(frm_component)
+		opts_menu.pack(side=tk.LEFT)
+		
+		def do_buy():
+			target_type, target_idx = opts_menu.value_as_target()
+			if target_type is None:
+				self.write_output("Select a valid option first")
+				return
+			
+			try:
+				msg = self.g.buy('instance', target_type, target_idx)
+			except RulesViolationError as ex:
+				self.write_output(str(ex))
+				return
+			
+			self.write_output(msg)
+			self.g.update()
+			self.write_main_content(self.g.status())
+
+		entry_click_lbl = tk.Button(frm_component, text="Buy", command=do_buy)
+		entry_click_lbl.pack(side=tk.LEFT)
+		
+		
 def run_gui(g: Engine):
-    global mode, modeBtnVar
-    
-    root = tk.Tk()
-    root.title("Cre8or Forge v0.0a")
-    
-    modeBtnVar = tk.StringVar()
-    modeBtnVar.set("Store")
-    mode = "status"
-
-    root.rowconfigure(0, minsize=300, weight=1)
-    root.columnconfigure(0, minsize=400, weight=1)
-    root.rowconfigure(1, minsize=100, weight=0)
-    
-    frm_output = tk.Frame(master=root)
-    output_height = 7
-    output = tk.Text(master=frm_output, height=output_height, width=103)
-    output.config(state=tk.DISABLED)
-    
-    def write_output(text: str):
-        output.config(state=tk.NORMAL)
-        output.delete("0.0", tk.END)
-        output.insert("0.0", text)
-        output.config(state=tk.DISABLED)
-    
-    output.pack()
-
-    frm_status = tk.Frame(master=root, relief=tk.SUNKEN, borderwidth=3)
-    frm_status.grid(row=0, column=0, sticky="nsew")
-    status = tk.Text(master=frm_status)
-    status_sb = ttk.Scrollbar(master=frm_status, command=status.yview)
-    status_sb.pack(side=tk.RIGHT, fill="y")
-    status['yscrollcommand'] = status_sb.set
-    
-    def write_status(text: str):
-        status.config(state=tk.NORMAL)
-        status.delete("0.0", tk.END)
-        status.insert("0.0", text)
-        status.config(state=tk.DISABLED)
-    
-    status.pack(side=tk.RIGHT, fill="x")
-
-    frm_entry = tk.Frame(master=root)
-    frm_entry.grid(row=0, column=1, sticky="nsew")
-
-    build_click_component(g, write_output, write_status, frm_entry)
-    build_buy_component(g, write_output, write_status, frm_entry)
-    build_mode_component(g, write_output, write_status, frm_entry)
-    
-    frm_output.grid(row=1, column=0, columnspan=2, sticky="nsew")
-
-    root.update()
-    root.minsize(root.winfo_width(), root.winfo_height())
 
     def update_status():
         global mode
