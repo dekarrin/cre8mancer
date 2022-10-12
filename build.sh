551391e1e0bbeca0dbf6444897d657160d3cd553
dekarrin <dekarrin.irl@gmail.com>
2022-10-12 09:35:23 -0500

    Add brief explain to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # use pyinstaller to create the executable
+#
+#
+# make shore you have done `pip install -U pyinstaller` before running this!
 
 pyinstaller launchgui.py --name cre8orforge
